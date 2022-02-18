ec3d373df718fee0d8fc3d36182afd3d74bede0f
Opaul Chanlongsawettakun <Opaul.chan@gmail.com>
2022-02-18 14:37:26 +0700

    edit calAge()

--- intro-to-js/opaul.js
+++ intro-to-js/opaul.js
@@ -9,11 +9,14 @@ function helloWorld() {
        alert(h1message + ' "วันนี้ไม่มีแรง อนุญาตให้แซงไปก่อน !" ');
     }
 function calAge(){
-    const yearOfBirth = prompt("Please enter your  year of birth");
+    const yearOfBirth = prompt("Please enter your  year of birth in A.D.");
     const date = new Date()
     let year = date.getFullYear()
     const age = year - yearOfBirth;
     alert("ยินดีด้วย นายอยู่ในประเทศนี้มา " + age +" ปีแล้ว")
+    /*if(age < 0){
+        alert("Year of birth in A.D. only!!!")
+    }else{alert("ยินดีด้วย นายอยู่ในประเทศนี้มา " + age +" ปีแล้ว")}*/
 }
 function loadProfile() {
     let myName = "Opaul"
